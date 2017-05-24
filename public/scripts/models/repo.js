fba6d5bb65e5f6893278feead729b7e821b5ca38
JohnScarrow <johna@scarrows.com>
2017-05-24 14:52:33 -0700

    update

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -14,7 +14,7 @@ var app = app || {};
     $.ajax({
       url: `user/repos`,
       type: 'GET',
-      //headers: {'Authorization': `token ${githubToken}`}
+      headers: {'Authorization': `token ${githubToken}`}
     })
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
